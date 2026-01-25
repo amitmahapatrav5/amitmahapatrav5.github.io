5bf43c5180efa63ce001411b5824fe7062c1a6d7
Amit Kumar Mahapatra <mahapatraarhas200@gmail.com>
2026-01-25 13:47:35 +0530

    PDF Resume Almost Done

--- test.py
+++ test.py
@@ -8,16 +8,44 @@ def render(json_file: Path, template_file: Path, output_file: Path):
     data = {}
     with json_file.open() as f:
         data = safe_load(f)
+
+    projects = data.get("projects", [])
+    experience = data.get("experience", [])
+
+    projects_by_org = {}
+
+    for project in projects:
+        org = project.get("organization")
+        name = project.get("name")
+
+        if not org or not name:
+            continue
+
+        projects_by_org.setdefault(org, []).append({ #type: ignore
+            "name": name,
+            "url": project.get("url", "")
+        })
+
+
+    for exp in experience:
+        org = exp.get("organization")
+
+        if not org:
+            exp["computed_highlights"] = []
+            continue
+
+        exp["computed_highlights"] = projects_by_org.get(org, []) # type: ignore
+
     env = Environment(
         loader=FileSystemLoader(str(template_file.parent)),
         autoescape=select_autoescape(['html','xml'])
     )
     tmpl = env.get_template(template_file.name)
-    
+
     rendered = tmpl.render(resume=data)
     with output_file.open("w", encoding="utf-8") as out:
         out.write(rendered)
-    print(f"Rendered {output_file}")
+
 
 if __name__ == "__main__":
     json_file = Path().cwd() / 'resume-wip.yaml'
