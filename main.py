040e185bc8ea91c118f5acf8bbc2b5ac40218ba6
Amit Kumar Mahapatra <mahapatraarhas200@gmail.com>
2026-01-31 11:51:38 +0530

    restructuring and finalizing resume.yaml and main.py and pdf version generation

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
-import sys
+from yaml import safe_load
 from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def render(json_file, template_file, output_file):
-    json_file = Path(json_file)
-    template_file = Path(template_file)
-    output_file = Path(output_file)
+def render(json_file: Path, template_file: Path, output_file: Path):
     data = {}
     with json_file.open() as f:
-        data = __import__('json').load(f)
+        data = safe_load(f)
+
+    projects = data.get("projects", [])
+    experience = data.get("experience", [])
+
+    projects_by_org = {}
+
+    for project in projects:
+        org = project.get("organization")
+        name = project.get("name")
+
+        if not org or not name:
+            continue
+
+        projects_by_org.setdefault(org, []).append({ #type: ignore
+            "name": name,
+            "url": project.get("url", "")
+        })
+
+
+    for exp in experience:
+        org = exp.get("organization")
+
+        if not org:
+            exp["computed_highlights"] = []
+            continue
+
+        exp["computed_highlights"] = projects_by_org.get(org, []) # type: ignore
+
     env = Environment(
         loader=FileSystemLoader(str(template_file.parent)),
         autoescape=select_autoescape(['html','xml'])
     )
     tmpl = env.get_template(template_file.name)
-    
+
     rendered = tmpl.render(resume=data)
     with output_file.open("w", encoding="utf-8") as out:
         out.write(rendered)
-    print(f"Rendered {output_file}")
+
 
 if __name__ == "__main__":
-    json_file = Path().cwd() / 'resume.json'
-
-    html_template_file = Path().cwd() / 'html_template.html'
-    html_output_file = Path().cwd() / 'index.html'
-    render(json_file, html_template_file, html_output_file)
+    json_file = Path().cwd() / 'resume.yaml'
 
     pdf_template_file = Path().cwd() / 'pdf_template.html'
     pdf_output_file = Path().cwd() / 'resume.pdf.html'
